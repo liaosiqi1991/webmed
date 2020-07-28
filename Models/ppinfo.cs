bf27719832c2a900e504aac67f8e00fc3d64583e
liaosiqi1991 <189953647@qq.com>
2020-07-28 10:52:18 +0800

    test3

--- Models/ppinfo.cs
+++ Models/ppinfo.cs
@@ -9,5 +9,15 @@ namespace Webmedrec.Models
     {
         public string 住院号 { get; set; }
         public string 病人id { get; set; }
+
+        public string 住院号1 { get; set; }
+        public string 病人id1 { get; set; }
+
+        public string 住院号2 { get; set; }
+        public string 病人id2 { get; set; }
+        public string 住院号3 { get; set; }
+        public string 病人id3 { get; set; }
+        public string 住院号4 { get; set; }
+        public string 病人id4 { get; set; }
     }
 }
