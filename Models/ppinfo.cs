b6454f3dd540e4a8408e794e80df1301e803542d
liaosiqi647 <189953647@qq.com>
2020-07-28 10:35:06 +0800

    test2

---
+++ Models/ppinfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webmedrec.Models
+{
+    public class ppinfo
+    {
+        public string 住院号 { get; set; }
+        public string 病人id { get; set; }
+    }
+}
